b6784deb4edc21b4953aabedd797c2b01319f779
Liber Dovat <liber.dovat@gmail.com>
2016-05-05 13:43:46 -0300

    primera versión del mapa topológico de Montevideo

---
+++ js/load.js
@@ -0,0 +1,80 @@
+// https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png
+// http://alt.opentopomap.org/{z}/{x}/{y}.png
+// https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+// http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpeg
+// http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png
+// http://otile1-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg
+// http://irs.gis-lab.info/?layers=landsat&request=GetTile&z={z}&x={x}&y={y}
+// http://tile.lonvia.de/hiking/{z}/{x}/{y}.png
+// http://tile.lonvia.de/cycling/{z}/{x}/{y}.png
+// http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png
+// https://[abc].tile.thunderforest.com/landscape/{z}/{x}/{y}.png http://www.thunderforest.com/maps/landscape/
+// https://[abc].tile.thunderforest.com/outdoors/{z}/{x}/{y}.png http://www.thunderforest.com/maps/outdoors/
+// https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png
+
+var map = L.map('map');
+
+var OpenStreetMap_Mapnik = L.tileLayer('http://otile1-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+  attribution: '&copy; <a href="https://opentopomap.org/about">OpenTopoMap</a>'
+});
+OpenStreetMap_Mapnik.addTo(map);
+
+map.setView([-34.9159, -56.16202], 14);
+
+// var SLDStyler = new L.SLDStyler("./assets/estilo_topo.sld");
+
+// var mvdo_geojson = new L.GeoJSON.AJAX("./assets/montevideo_topo.geojson", SLDStyler.getStyleFunction());
+// mvdo_geojson.addTo(map);
+
+function getColor(elev) {
+    return elev > 70 ? '#A90201' :
+           elev > 60 ? '#8E5629' :
+           elev > 50 ? '#739A51' :
+           elev > 40 ? '#59C879' :
+           elev > 30 ? '#3EC3A1' :
+           elev > 20 ? '#239BC9' :
+           elev > 10 ? '#0927F1' :
+           elev > 4  ? '#0001FF' :
+           elev <= 4 ? '#0000ff' :
+                       '#FFEDA0';
+}
+
+function getColor2(elev) {
+    return elev > 70 ? '#800026' :
+           elev > 60 ? '#BD0026' :
+           elev > 50 ? '#E31A1C' :
+           elev > 40 ? '#FC4E2A' :
+           elev > 30 ? '#FD8D3C' :
+           elev > 20 ? '#FEB24C' :
+           elev > 10 ? '#FED976' :
+                       '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+function style2(feature) {
+    return {
+        weight: 1,
+        opacity: 0.6,
+        color: getColor(feature.properties.ELEV)
+    };
+}
+
+function style3(feature) {
+    return {
+        weight: 1,
+        opacity: 0.6,
+        color: getColor2(feature.properties.ELEV)
+    };
+}
+
+var mvdo_geojson = new L.GeoJSON.AJAX("./assets/montevideo_topo.geojson", {style: style2});
+mvdo_geojson.addTo(map);
